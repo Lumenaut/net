0a1cd3858ecab5d74190b40cba868b22f9375319
Lumenaut <33041885+Lumenaut@users.noreply.github.com>
2018-01-16 17:31:56 +0000

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -127,8 +127,8 @@ $(document).ready(() => {
 
 // clipboard
 $(document).ready(() => {
-	var clipboard = new Clipboard('#copy-address-link');
-	const $addrLink = $('#copy-address-link');
+	var clipboard = new Clipboard('.copy-address');
+	const $addrLink = $('.copy-address');
 	$addrLink.click((e) => {
 		e.preventDefault();
 		$addrLink.addClass('visited');
