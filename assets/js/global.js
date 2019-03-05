7a3957128ac68c524622ac025717724b9ee50554
Lumenaut <33041885+Lumenaut@users.noreply.github.com>
2019-03-05 00:30:14 +0000

    Update global.js

--- assets/js/global.js
+++ assets/js/global.js
@@ -55,7 +55,7 @@ $(document).ready(function(){
 
 	table.on( 'click', '.result', function() {
 		var data = table.row($(this).parents('tr')).data();
-		window.open(data[ 5 ], '_blank');
+		window.open(data[ 3 ], '_blank');
 	});
 
 
